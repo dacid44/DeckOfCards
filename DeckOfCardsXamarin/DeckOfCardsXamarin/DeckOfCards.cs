212652a671f67d15010df0d45962b5b31cd76d18
dacid44 <dacid44@gmail.com>
2021-04-05 10:04:38 -0500

    Added Xamarin implementation

---
+++ DeckOfCardsXamarin/DeckOfCardsXamarin/DeckOfCards.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace DeckOfCardsXamarin {
+   public class DeckOfCards {
+      public readonly bool Success;
+      public readonly string DeckID;
+
+      public bool IsShuffled { get; private set; }
+      public int CardsRemaining { get; private set; }
+      public string ErrorMessage { get; private set; }
+
+      private List<Card> _drawnCards;
+      public List<Card> DrawnCards => _drawnCards.AsReadOnly().ToList();
+
+      public DeckOfCards(int deckCount = 1) {
+         JsonResponse response;
+         try {
+            response = new JsonResponse(
+               "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + deckCount);
+         } catch (WebException e) {
+            Success = false;
+            ErrorMessage = e.Message;
+            return;
+         }
+         Success = (bool) (response.Json["success"] ?? false);
+         if (!Success) { return; }
+         
+         DeckID = (string) response.Json["deck_id"];
+         IsShuffled = (bool) (response.Json["shuffled"] ?? false);
+         CardsRemaining = (int) response.Json["remaining"];
+
+         _drawnCards = new List<Card>();
+      }
+
+      public List<Card> Draw(int count) {
+         JsonResponse response;
+         try {
+            response = new JsonResponse(
+               "https://deckofcardsapi.com/api/deck/" + DeckID + "/draw/?count=" + count);
+         } catch (WebException e) {
+            ErrorMessage = e.Message;
+            return null;
+         }
+         if (!(bool) (response.Json["success"] ?? false)) { return null; }
+         
+         JArray cards = (JArray) response.Json["cards"];
+         List<Card> toReturn = new List<Card>();
+         foreach (JObject cardObj in cards) {
+            toReturn.Add(new Card(cardObj));
+         }
+         _drawnCards.AddRange(toReturn);
+         
+         CardsRemaining = (int) response.Json["remaining"];
+         return toReturn;
+      }
+
+      public bool Shuffle() {
+         JsonResponse response;
+         try {
+            response = new JsonResponse("https://deckofcardsapi.com/api/deck/" + DeckID + "/shuffle");
+         } catch (WebException e) {
+            ErrorMessage = e.Message;
+            return false;
+         }
+         if (!(bool) (response.Json["success"] ?? false)) { return false; }
+         
+         IsShuffled = (bool) (response.Json["shuffled"] ?? false);
+         CardsRemaining = (int) response.Json["remaining"];
+         _drawnCards.Clear();
+         return true;
+      }
+   }
+
+   public readonly struct Card {
+      public readonly string Code;
+      public readonly string SvgImage;
+      public readonly string PngImage;
+      public readonly string Value;
+      public readonly string Suit;
+      
+      public Card(JObject cardObj) {
+         Code = (string) cardObj["code"];
+         SvgImage = (string) cardObj["images"]["svg"];
+         PngImage = (string) cardObj["images"]["png"];
+
+         TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+         if (!(cardObj["images"] is null)) {
+            Value = ti.ToTitleCase(ti.ToLower((string) cardObj["value"] ?? ""));
+         } else {
+            Value = null;
+         }
+         if (!(cardObj["images"] is null)) {
+            Suit = ti.ToTitleCase(ti.ToLower((string) cardObj["suit"] ?? ""));
+         } else {
+            Suit = null;
+         }
+      }
+      public Card(string code, string svgImage, string pngImage, string value, string suit) {
+         Code = code;
+         SvgImage = svgImage;
+         PngImage = pngImage;
+         Value = value;
+         Suit = suit;
+      }
+
+      public override string ToString() => $"{Value} of {Suit}";
+   }
+   
+   class JsonResponse {
+      public readonly JObject Json;
+      
+      public JsonResponse(string url) {
+         WebRequest request = WebRequest.Create(url);
+         request.ContentType = "Application/JSON";
+         var response = (HttpWebResponse) request.GetResponse();
+         var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+         Json = JObject.Parse(responseString);
+      }
+   }
+}
---
+++ DeckOfCardsXamarin/DeckOfCardsXamarin/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeckOfCardsXamarin {
+   public partial class MainPage : ContentPage {
+      private static readonly Regex _numerals = new Regex("[^0-9]+");
+      
+      private DeckOfCards _deck;
+      
+      public MainPage() {
+         InitializeComponent();
+         _deck = new DeckOfCards();
+         if (_deck.Success) {
+            DeckIDField.Text = "Deck ID: " + _deck.DeckID;
+         } else {
+            MessageField.Text = _deck.ErrorMessage;
+         }
+      }
+      
+      private void OnDrawInputChanged(object sender, TextChangedEventArgs e) {
+         if (NumCards.Text.Length >= 4 || _numerals.IsMatch(e.NewTextValue)) {
+            ((Entry) sender).Text = e.OldTextValue;
+         }
+      }
+      
+      private void DrawEvent(object sender, EventArgs e) {
+         List<Card> drawnCards = _deck.Draw(int.Parse(NumCards.Text == "" ? "0" : NumCards.Text));
+         if (drawnCards is null) {
+            MessageField.Text = _deck.ErrorMessage;
+            return;
+         }
+         DisplayCards(drawnCards);
+         MessageField.Text = $"Drew {drawnCards.Count} cards, {_deck.CardsRemaining} cards remaining.";
+      }
+
+      private void ShuffleEvent(object sender, EventArgs e) {
+         bool success = _deck.Shuffle();
+         if (!success) {
+            MessageField.Text = _deck.ErrorMessage;
+            return;
+         }
+         DisplayCards(new List<Card>());
+         MessageField.Text = $"Shuffled the deck, {_deck.CardsRemaining} cards remaining.";
+      }
+
+      private void ListEvent(object sender, EventArgs e) {
+         if (!_deck.Success) {
+            MessageField.Text = "Deck initialization returned an error: " + _deck.ErrorMessage;
+            return;
+         }
+         DisplayCards(_deck.DrawnCards);
+         MessageField.Text =
+            $"{_deck.DrawnCards.Count} cards have been drawn, and there are {_deck.CardsRemaining} cards remaining.";
+      }
+      
+      private void DisplayCards(List<Card> cards) {
+         int rows = 0;
+         for (; rows * CalcMaxCardRow(rows) < cards.Count; rows++) { }
+         int cardsPerRow = CalcMaxCardRow(rows);
+         
+         CardGrid.RowDefinitions.Clear();
+         CardGrid.Children.Clear();
+         for (int i = 0; i < rows; i++) {
+            int numCards = Math.Min(cardsPerRow, cards.Count - (i * cardsPerRow));
+            if (numCards <= 0) { break; }
+            CardGrid.RowDefinitions.Add(new RowDefinition());
+            Grid row = new Grid();
+            Grid.SetRow(row, i);
+            CardGrid.Children.Add(row);
+            for (int j = 0; j < numCards; j++) {
+               row.ColumnDefinitions.Add(new ColumnDefinition());
+               Image img = new Image();
+               img.Source = new UriImageSource {
+                  Uri = new Uri(cards[i * cardsPerRow + j].PngImage, UriKind.Absolute),
+                  CachingEnabled = true,
+                  CacheValidity = new TimeSpan(0, 30, 0)
+               };
+               Grid.SetColumn(img, j);
+               row.Children.Add(img);
+            }
+         }
+      }
+      
+      private int CalcMaxCardRow(int rows) {
+         if (rows == 0) { return 0; }
+         double gridWidth = CardGrid.Width;
+         double gridHeight = CardGrid.Height;
+         return (int) Math.Floor(gridWidth / (gridHeight / rows) * (4 / 3.0));
+      }
+   }
+}
